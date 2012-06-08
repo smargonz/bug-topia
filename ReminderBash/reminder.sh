3375b52cdcef945feca76c8bb1a14fcf19289d20
smargonz <smar.gonz@gmail.com>
2012-06-08 17:40:35 -0600

    Updating modification time and number of compiles in modLogFile

--- ReminderBash/reminder.sh
+++ ReminderBash/reminder.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
-STR=`stat -f %m test1.txt`
-echo $STR
+fileName=test4.txt
 
-#if logFile exists
+# most recent modified time
+currModTime=`stat -f %m $fileName`
+
+#if modLog exists
 if [ -f ./modLogFile.txt ] 
 then
-   echo "EXISTS!"
-   #if logFile.contains( fileName )
-    #if [ grep -Fxq "test1.txt" modLogFile.txt ] then
-	
+   
+   IN_MOD_LOG=`grep -a "$fileName" modLogFile.txt`
 
-       # if currModTime > savedModTime  
-          # savedModTime = currModTime
-          # ++ count
-   #else
-       # append fileName
-       # ex: myFile modTime 0
-   # fi
-   #else
+   #if modLog does NOT contain file
+   if [ -z "$IN_MOD_LOG" ]
+   then
+#       echo "ADDING $fileName TO MOD LOG"
+       echo "$fileName $STR 0">>modLogFile.txt 
+   else
+#       echo "FOUND YOUR FILE"
+       lastModTime=`grep -a $fileName modLogFile.txt | awk '{print $2}'`
+
+#       echo $currModTime
+
+       # if currModTime > savedModTime ...aka was updated  
+       if [ "$currModTime" -gt "$lastModTime" ]
+       then
+
+        numCompiles=`grep -a $fileName modLogFile.txt | awk '{print $3}'`
+	numCompiles=$(($numCompiles+1))
+#	echo $numCompiles
+
+	sed -i -e "/$fileName/s/\(.*\) [0-9]* /\1 $currModTime /" modLogFile.txt
+	sed -i -e "/$fileName/s/\(.*\) [0-9].*/\1 $numCompiles/" modLogFile.txt
+       fi
+    fi
+else
+
    # create logfile
-   #echo "DOES NOT EXIST"
+   echo "CREATING MODLOG"
+   echo "$fileName $STR 0">>modLogFile.txt 
 fi

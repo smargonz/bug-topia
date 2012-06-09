8277c92d6bb163adfef150847149ae155122f76b
smargonz <smar.gonz@gmail.com>
2012-06-09 12:12:55 -0600

    Can now specify directory to monitor. Directory path stored in modLogFile

--- ReminderBash/reminder.sh
+++ ReminderBash/reminder.sh
@@ -2,53 +2,60 @@
 
 IFS='
 '
-array=(`ls -1`)
-
-len=${#array[*]}
-i=0
-while [ $i -lt $len ]
-do
-
-fileName=${array[$i]}
-
-# most recent modified time
-currModTime=`stat -f %m $fileName`
 
 #if modLog exists
 if [ -f ./modLogFile.txt ] 
 then
-   
-   IN_MOD_LOG=`grep -a "$fileName" modLogFile.txt`
-
-   #if modLog does NOT contain file
-   if [ -z "$IN_MOD_LOG" ]
-   then
-#       echo "ADDING $fileName TO MOD LOG"
-       echo "$fileName $STR 0">>modLogFile.txt 
-   else
-#       echo "FOUND YOUR FILE"
-       lastModTime=`grep -a $fileName modLogFile.txt | awk '{print $2}'`
-
-#       echo $currModTime
-
-       # if currModTime > savedModTime ...aka was updated  
-       if [ "$currModTime" -gt "$lastModTime" ]
-       then
-
-        numCompiles=`grep -a $fileName modLogFile.txt | awk '{print $3}'`
-	numCompiles=$(($numCompiles+1))
-#	echo $numCompiles
-
-	sed -i -e "/$fileName/s/\(.*\) [0-9]* /\1 $currModTime /" modLogFile.txt
-	sed -i -e "/$fileName/s/\(.*\) [0-9].*/\1 $numCompiles/" modLogFile.txt
-       fi
-    fi
+    #get directory path
+    DIRPATH=$(head -n 1 modLogFile.txt)
 else
-
    # create logfile
-   echo "CREATING MODLOG"
-   echo "$fileName $currModTime 0">>modLogFile.txt 
+   echo "CREATING MODLOG FOR [specify full path]:"
+   read -e DIRPATH
+   echo $DIRPATH>>modLogFile.txt
 fi
+
+#read directory contents into array 
+array=(`ls -1 $DIRPATH`)
+
+len=${#array[*]}
+
+#Compare mod times against logged mod 
+# time for each file
+
+i=0
+while [ $i -lt $len ]
+do
+
+    fileName=${array[$i]}
+
+    #most recent modified time
+    currModTime=`stat -f %m $fileName`
+
+    #get file's entry in modlog
+    MOD_LOG_ENTRY=`grep -a "$fileName" modLogFile.txt`
+
+     #if modLog does NOT contain file's entry
+    if [ -z "$MOD_LOG_ENTRY" ]
+    then
+        #add it
+	echo "$fileName $currModTime 0">>modLogFile.txt 
+    else 
+        #Check if update required.
+        #Grab saved mod time.
+	lastModTime=`grep -a $fileName modLogFile.txt | awk '{print $2}'`
+
+        #if currModTime > savedModTime ...ie file was updated  
+	if [ "$currModTime" -gt "$lastModTime" ]
+	then
+
+            numCompiles=`grep -a $fileName modLogFile.txt | awk '{print $3}'`
+	    numCompiles=$(($numCompiles+1))
+
+	    sed -i -e "/$fileName/s/\(.*\) [0-9]* /\1 $currModTime /" modLogFile.txt
+	    sed -i -e "/$fileName/s/\(.*\) [0-9].*/\1 $numCompiles/" modLogFile.txt
+	fi
+    fi
 
 let i++
 done
